8e5bae2679bc2a7c78578a469f0a9fd6d8f0a895
CWR4 <43339278+CWR4@users.noreply.github.com>
2019-05-27 10:38:19 +0200

    delete form and functionality implemented; css updated for form;

---
+++ src/Form/DeleteMovienightType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\MovieNight;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DeleteMovienightType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date', DateType::class, ['format' => 'dd MM yyyy', 'label' => false, 'disabled' => true, 'attr' => ['read_only' => true]])
+            ->add('time', TimeType::class, ['label' => false, 'disabled' => true])
+            ->add('location', TextType::class, ['label' => false, 'disabled' => true])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => MovieNight::class,
+            'disabled' => true,
+        ]);
+    }
+}
