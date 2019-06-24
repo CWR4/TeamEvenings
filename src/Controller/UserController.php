3a28535ba2f95f65a995228b41e33bc65731d885
CWR4 <43339278+CWR4@users.noreply.github.com>
2019-06-24 12:49:36 +0200

    password can be changed with typing in old and new password;

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,12 +8,15 @@ use App\Form\ChangePasswordType;
 use App\Form\ChangeUsernameType;
 use App\Form\DeleteUserType;
 use App\Form\SetUserRoleType;
+use App\Security\AppAuthenticator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use function Sodium\add;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Class UserController
@@ -145,14 +148,27 @@ class UserController extends AbstractController
      * @return Response
      * @Route("/user/changepassword/{user<\d+>?}", name="change_password")
      */
-    public function changePassword(Request $request, User $user): Response
+    public function changePassword(UserPasswordEncoderInterface $encoder, Request $request, User $user): Response
     {
         $form = $this->createForm(ChangePasswordType::class);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
-            dump('Yessir');
-            dump($form->getData());
+            if($encoder->isPasswordValid($user, $form->getData()->getPassword()))
+            {
+                $user->setPassword($encoder->encodePassword($user, $form->get('newPassword')->getData()));
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash('success', 'Passwort erfolgreich geändert!');
+
+                return $this->redirectToRoute('edit_user', ['id' => $user->getId()]);
+            }
+            else
+            {
+                dump('Passwort falsch');
+            }
         }
 
         return $this->render('user/changePassword.html.twig', [
